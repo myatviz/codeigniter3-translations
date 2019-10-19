bba44701c8cecba9deb1fca6ed6c7bf07cf334b3
Min Myat Oo <myatviz@users.noreply.github.com>
2019-10-19 13:27:09 +0700

    Commit two Burmese translation files.

---
+++ language/burmese/calendar_lang.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cal_su'] = 'တ';
+$lang['cal_mo'] = 'တ';
+$lang['cal_tu'] = 'အ';
+$lang['cal_we'] = 'ဗ';
+$lang['cal_th'] = 'က';
+$lang['cal_fr'] = 'သ';
+$lang['cal_sa'] = 'စ';
+$lang['cal_sun'] = 'နွေ';
+$lang['cal_mon'] = 'လာ';
+$lang['cal_tue'] = 'ဂါ';
+$lang['cal_wed'] = 'ဟူး';
+$lang['cal_thu'] = 'တေး';
+$lang['cal_fri'] = 'ကြာ';
+$lang['cal_sat'] = 'နေ';
+$lang['cal_sunday'] = 'တနင်္ဂနွေ';
+$lang['cal_monday'] = 'တနင်္လာ';
+$lang['cal_tuesday'] = 'အင်္ဂါ';
+$lang['cal_wednesday'] = 'ဗုဒ္ဓဟူး';
+$lang['cal_thursday'] = 'ကြာသပတေး';
+$lang['cal_friday'] = 'သောကြာ';
+$lang['cal_saturday'] = 'စနေ';
+$lang['cal_jan'] = 'ဇန်';
+$lang['cal_feb'] = 'ဖေ';
+$lang['cal_mar'] = 'မတ်';
+$lang['cal_apr'] = 'ဧ';
+$lang['cal_may'] = 'မေ';
+$lang['cal_jun'] = 'ဇွန်';
+$lang['cal_jul'] = 'ဇူ';
+$lang['cal_aug'] = 'ဩ';
+$lang['cal_sep'] = 'စက်';
+$lang['cal_oct'] = 'အောက်';
+$lang['cal_nov'] = 'နို';
+$lang['cal_dec'] = 'ဒီ';
+$lang['cal_january'] = 'ဇန်နဝါရီ';
+$lang['cal_february'] = 'ဖေဖော်ဝါရီ';
+$lang['cal_march'] = 'မတ်';
+$lang['cal_april'] = 'ဧပြီ';
+$lang['cal_mayl'] = 'မေ';
+$lang['cal_june'] = 'ဇွန်';
+$lang['cal_july'] = 'ဇူလှိုင်';
+$lang['cal_august'] = 'ဩဂုတ်';
+$lang['cal_september'] = 'စင်တင်ဘာ';
+$lang['cal_october'] = 'အောက်တိုဘာ';
+$lang['cal_november'] = 'နိုဝင်ဘာ';
+$lang['cal_december'] = 'ဒီဇင်ဘာ';
